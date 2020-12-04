4fb0c8f8c3cb504198f361e99aeb38c9d00c6b27
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2020-12-04 11:08:59 +0330

    Setup Tests

---
+++ tests/OptionsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hsy\Options\Tests;
+
+
+class OptionsTest extends TestCase
+{
+    /** @test */
+    public function testInstance()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Hsy\Options\Tests;
+
+
+use Hsy\Options\OptionsServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            OptionsServiceProvider::class
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('app.locale', 'fa');
+        $app['config']->set('app.faker_locale', 'fa_IR');
+        $app['config']->set('app.timezone', 'Asia/tehran');
+        $app['config']->set('database.default', 'testdb');
+        $app['config']->set('database.connections.testdb', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+    }
+}
