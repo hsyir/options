33a357a9574ffdf95f9636956cc6b9616f3f45d7
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2021-03-13 23:45:56 +0330

    Update options.php

--- config/options.php
+++ config/options.php
@@ -3,6 +3,31 @@ return [
     'optionsModel' => \Hsy\Options\Models\Option::class,
     'groups' =>
         [
-           
+            'group-one' => [
+                'title' => 'title of group one',
+                'fields' => [
+                    [
+                        'key' => 'field-one',
+                        'title' => 'field title one',
+                        'type' => 'text',
+                    ],
+                    [
+                        'key' => 'field-two',
+                        'title' => 'field-title-two',
+                        'type' => 'text',
+                        'description' => 'Some descriptions',
+                    ],
+                ]
+            ],
+            'group-two' => [
+                'title' => 'title of group one',
+                'fields' => [
+                    [
+                        'key' => 'field-one',
+                        'title' => 'field title one',
+                        'type' => 'text',
+                    ],
+                ]
+            ],
         ]
 ];
